cca7cc9f75994e98573fbe8331eb06a2eda6fc6f
Andreea <andelenasamur@gmail.com>
2022-11-02 12:29:41 +0100

    handle change

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 const Form = () => {
+    
+    const [inputValues, setInputValues] = useState({
+        nick: '',
+        subMonths: 0,
+        avatar: '',
+        description: ''
+    })
 
     const handleSubmit = () => {}
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInputValues({
+            ...inputValues,
+            [event.target.name]: event.target.value
+        })
+    }
 
         return(
             <div>
                 <form onSubmit={handleSubmit}>
-                <input type='text' name='nick' placeholder='nick'/>
-                <input type='text' name='subMonths' placeholder='subMonths'/>
-                <input type='text' name='avatar' placeholder='avatar'/>
-                <input type='text' name='description' placeholder='description'/>
+                <input onChange={handleChange} value={inputValues.nick} type='text' name='nick' placeholder='nick'/>
+                <input onChange={handleChange} value={inputValues.subMonths} type='number' name='subMonths' placeholder='subMonths'/>
+                <input onChange={handleChange} value={inputValues.avatar} type='text' name='avatar' placeholder='avatar'/>
+                <textarea onChange={handleChange} value={inputValues.description} name='description' placeholder='description'/>
                 <button>Save new sub!</button>
                 </form>
             </div>
