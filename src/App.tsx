3bb0de2668c4f4f9e43e3748eff21b564f2fe16b
Andreea <andelenasamur@gmail.com>
2022-11-03 08:31:58 +0100

    using the useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
@@ -27,16 +27,21 @@ function App() {
 
   const [subs, setSubs] = useState<AppState["subs"]>([])
   const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
+  const divRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     setSubs(INITIAL_STATE)
   }, [])
 
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+  } 
+
   return (
-    <div className="App">
+    <div className="App" ref={divRef}>
       <h1>Midu subs</h1>
         <List subs={subs}/>
-        <Form />
+        <Form onNewSub={handleNewSub} />
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
 import Sub from '../types';
+import useNewSubForm from '../hooks/useNewSubForm';
 
-interface FormState {
-    inputValues: Sub
+interface FormProps {
+    onNewSub: (newSub: Sub) => void
 }
 
-const Form = () => {
+const Form = ({ onNewSub }: FormProps ) => {
     
-    const [inputValues, setInputValues] = useState<FormState["inputValues"]>({
-        nick: '',
-        subMonths: 0,
-        avatar: '',
-        description: ''
-    })
+    // const [inputValues, setInputValues] = useState<FormState["inputValues"]>(INITIAL_STATE)
 
-    const handleSubmit = () => {}
+    const [inputValues, dispatch] = useNewSubForm()
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        onNewSub(inputValues)
+        handleClear()
+    }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setInputValues({
-            ...inputValues,
-            [event.target.name]: event.target.value
+        const {name, value} = event.target
+
+        dispatch({
+            type: "change_value",
+            payload: {
+                inputName: name,
+                inputValue: value
+            }
         })
+    }
+
+    const handleClear = () => {
+        dispatch({type: "clear"})
     }
 
         return(
@@ -30,7 +40,8 @@ const Form = () => {
                 <input onChange={handleChange} value={inputValues.subMonths} type='number' name='subMonths' placeholder='subMonths'/>
                 <input onChange={handleChange} value={inputValues.avatar} type='text' name='avatar' placeholder='avatar'/>
                 <textarea onChange={handleChange} value={inputValues.description} name='description' placeholder='description'/>
-                <button>Save new sub!</button>
+                <button onClick={handleClear} type='button'>Clear the form</button>
+                <button type='submit'>Save new sub!</button>
                 </form>
             </div>
         )
